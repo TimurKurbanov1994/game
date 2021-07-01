37d2caae9ce3ce495141b01c5d54b9263edead76
Timur <kurbanov.t.s.94@gmail.com>
2021-07-02 00:02:55 +0500

    second commit

--- src/game.js
+++ src/game.js
@@ -1,63 +1,53 @@
+const colors = ["tomato", "skyblue", "violet", "orange", "palegreen"];
 
-
-export function startGame(argTimer) {
-    setInterval(decreaseTime, 1000);
-    createRandomCircle();
-    setTime(argTimer);
-}
-
-function decreaseTime(argTimer) {
-    if (argTimer === 0) {
-        finishGame();
-    } else {
-        let current = --argTimer;
-        if (current < 10) {
-            current = `0${current}`;
-        }
-        setTime(current);
-    }
-}
-
-function setTime(value) {
-    timeEl.textContent = `00:${value}`;
-}
-
-function finishGame() {
-    timeEl.parentNode.classList.add("hide");
-    board.innerHTML = `<h1 class="animate__animated animate__pulse">Ваш счет: ${score}</h1>
-    <button class="time-btn play-btn animate__animated animate__bounce" id="play-btn">Сыграть еще раз</button>`;
-    const playButton = document.querySelector("#play-btn");
-    playButton.addEventListener("click", () => {
-        document.location.reload();
-    });
-    setColor();
+export function finishGame(selectorTime, value) {
+  selectorTime.parentNode.classList.add("hide");
+  board.innerHTML = `<h1 class="animate__animated">Ваш счет: ${value}</h1>
+    <button class="time-btn play-btn" id="play-btn">Сыграть еще раз</button>`;
+  const playButton = document.querySelector("#play-btn");
+  playButton.addEventListener("click", () => {
+    document.location.reload();
+  });
+  setColor();
 }
 
 export function createRandomCircle() {
-    const circle = document.createElement("div");
-    circle.classList.add("circle", "animate__animated", "animate__zoomIn");
-    const size = getRandomNumber(15, 50);
-    const { width, height } = board.getBoundingClientRect();
-    const x = getRandomNumber(15, width - size);
-    const y = getRandomNumber(15, height - size);
-    circle.style.width = `${size}px`;
-    circle.style.height = `${size}px`;
-    circle.style.top = `${y}px`;
-    circle.style.left = `${x}px`;
-    circle.style.background = colors[getRandomNumber(0, colors.length)];
-    board.appendChild(circle);
+  const circle = document.createElement("div");
+  circle.classList.add("circle", "animate__animated", "animate__zoomIn");
+  const size = getRandomNumber(15, 50);
+  const { width, height } = board.getBoundingClientRect();
+  const x = getRandomNumber(15, width - size);
+  const y = getRandomNumber(15, height - size);
+  circle.style.width = `${size}px`;
+  circle.style.height = `${size}px`;
+  circle.style.top = `${y}px`;
+  circle.style.left = `${x}px`;
+  circle.style.background = colors[getRandomNumber(0, colors.length)];
+  board.appendChild(circle);
 }
 
 function getRandomNumber(min, max) {
-    return Math.round(Math.random() * (max - min) + min);
+  return Math.round(Math.random() * (max - min) + min);
 }
 
 function getRandomColor() {
-    const index = Math.floor(Math.random() * colors.length);
-    return colors[index];
+  const index = Math.floor(Math.random() * colors.length);
+  return colors[index];
 }
 
 function setColor() {
-    const startColor = getRandomColor();
-    return colors[startColor];
+  const startColor = getRandomColor();
+  return colors[startColor];
+}
+
+export function createModal(title, content) {
+  const modal = document.createElement("div");
+  modal.classList.add("modal");
+
+  const html = `
+      <h1>${title}</h1>
+      <div class="modal-content">${content}</div>
+    `;
+
+  modal.innerHTML = html;
 }
--- src/script.js
+++ src/script.js
@@ -1,36 +1,68 @@
-import './styles/style.css'
-import {startGame} from "./game"
+import "./styles/style.css";
+import { createRandomCircle, finishGame, createModal } from "./game";
 
 const startBtn = document.querySelector("#start");
 const screens = document.querySelectorAll(".screen");
 const timeList = document.querySelector("#time-list");
 const timeEl = document.querySelector("#time");
 const board = document.querySelector("#board");
+const btnModal = document.querySelector(".gradient-button");
 
-
-
-// let time = 0;
+let time = 0;
 let score = 0;
-const colors = ["tomato", "skyblue", "violet", "orange", "palegreen"];
 
 startBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    screens[0].classList.add("up");
+  e.preventDefault();
+  screens[0].classList.add("up");
 });
 
 timeList.addEventListener("click", (e) => {
-    if (e.target.classList.contains("time-btn")) {
-        let time = parseInt(e.target.getAttribute("data-time"));
-        screens[1].classList.add("up");
-        board.innerHTML = "";
-        startGame(time);
+  if (e.target.classList.contains("time-btn")) {
+    time = parseInt(e.target.getAttribute("data-time"));
+    screens[1].classList.add("up");
+    if (time < 10) {
+      timeEl.textContent = `00:0${time}`;
+    } else {
+      timeEl.textContent = `00:${time}`;
     }
+    startGame(time);
+  }
 });
 
 board.addEventListener("click", (e) => {
-    if (e.target.classList.contains("circle")) {
-        score++;
-        e.target.remove();
-        createRandomCircle();
-    }
+  if (e.target.classList.contains("circle")) {
+    score++;
+    e.target.remove();
+    createRandomCircle();
+  }
 });
+
+// btnModal.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   createModal("asdad", "adsasd");
+// });
+
+createModal("asdad", "adsasd");
+
+function startGame() {
+  board.innerHTML = "";
+  setInterval(decreaseTime, 1000);
+  createRandomCircle();
+}
+
+function decreaseTime() {
+  if (time === 0) {
+    finishGame(timeEl, score);
+    btnModal.style.display = "block";
+  } else {
+    let current = --time;
+    if (current < 10) {
+      current = `0${current}`;
+    }
+    setTime(current);
+  }
+}
+
+function setTime(value) {
+  timeEl.textContent = `00:${value}`;
+}
